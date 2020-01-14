68d2b5709aa189ba4160b0ec045d4d25389616d9
Ruben Ramirez <rubenieldroid@gmail.com>
2020-01-13 19:54:55 -0600

    Initial Commit

---
+++ faceidProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  faceidProject
+//
+//  Created by Ruben Ramirez on 1/13/20.
+//  Copyright © 2020 Ruben Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
